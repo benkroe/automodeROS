6cddd47cf03b57278cddd66c31e68f1589dd2449
ben.kroener <ben.kroener@uni-konstanz.de>
2025-09-28 12:13:04 +0200

    09

--- src/basic_modules/basic_modules/behaviors/behavior_exploration.py
+++ src/basic_modules/basic_modules/behaviors/behavior_exploration.py
@@ -18,7 +18,7 @@ class Behavior(BehaviorBase):
         self._turning_time = time.time()
 
         self._obstacle_threshold = 30  # Proximity magnitude threshold for obstacle detection
-        self._forward_speed = 0.5       # Forward movement speed
+        self._forward_speed = 0.3       # Forward movement speed
         self._turn_speed = 0.6          # Turning speed (will calibrate later)
 
     @staticmethod
