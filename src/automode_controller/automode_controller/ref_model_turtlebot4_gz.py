f59f11069843fda7c20358ab87ec6c89e0b1bb4f
ben.kroener <ben.kroener@uni-konstanz.de>
2025-09-28 17:32:34 +0200

    some speed changes

--- src/automode_controller/automode_controller/ref_model_turtlebot4_gz.py
+++ src/automode_controller/automode_controller/ref_model_turtlebot4_gz.py
@@ -22,13 +22,13 @@ class TurtleBot4ReferenceNode(Node):
     def __init__(self):
         super().__init__('turtlebot4_reference_node')
         # Publisher for cmd_vel (movement)
-        self._cmd_vel_pub = self.create_publisher(TwistStamped, 'cmd_vel', 10)
+        self._cmd_vel_pub = self.create_publisher(TwistStamped, 'cmd_vel', 20)
         # Publisher for RobotState
-        self._robot_state_pub = self.create_publisher(RobotState, 'robotState', 10)
+        self._robot_state_pub = self.create_publisher(RobotState, 'robotState', 20)
         # Subscriber for wheels_speed (from automode)
-        self.create_subscription(Float32MultiArray, 'wheels_speed', self._wheels_speed_cb, 10)
+        self.create_subscription(Float32MultiArray, 'wheels_speed', self._wheels_speed_cb, 20)
         # Subscribe for lidar scan (turtlebot4)
-        # self.create_subscription(LaserScan, 'scan', self._lidar_scan_cb, 10)
+        # self.create_subscription(LaserScan, 'scan', self._lidar_scan_cb, 0)
         # Subscribe to raw IR and light sensor topics and ground
         self.create_subscription(Float32MultiArray, 'ir_intensities', self._ir_cb, qos_profile_sensor_data)
         self.create_subscription(Float32, 'light_sensor_front_left', self._light_fl_cb, qos_profile_sensor_data)
