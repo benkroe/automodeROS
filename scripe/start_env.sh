07017f96e4a3deb90a90e84f1168803ac7de5078
ben.kroener <ben.kroener@uni-konstanz.de>
2025-09-30 16:06:03 +0200

    small changes at the end

--- scripe/start_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Go to robostack"
-cd ../robostack
-
-echo "Activate virtual environment jazzy"
-eval "$(pixi shell-hook -e jazzy)"
-
-echo "Go back to workspace"
-cd ../automode_ros_py
